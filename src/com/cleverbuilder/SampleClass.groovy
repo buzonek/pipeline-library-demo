8a2478a9f5179cc4ab958a0f78a27cba1955934c
buzonek <buzonekk@gmail.com>
2023-01-23 21:25:14 +0100

    test

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 package com.cleverbuilder
 
-class SampleClass {
-   String name
-   Integer age
+// class SampleClass {
+//    String name
+//    Integer age
 
-   def increaseAge(Integer years) {
-      this.age += years
-   }
-}
+//    def increaseAge(Integer years) {
+//       this.age += years
+//    }
+// }
 
 def sayHello(String name) {
    println name + " test"
